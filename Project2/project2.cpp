4e94d1a412979d4becdfab1ba0ec3bd83820f566
ivanf05 <ivanfonseca@lewisu.edu>
2017-09-30 16:15:20 -0500

    project2-linked_list

--- Project2/project2.cpp
+++ Project2/project2.cpp
@@ -1,20 +1,145 @@
 #include <iostream>
-#include "project2.hxx"
 using namespace std;
 
+class HeadNode{
+	//Node object
+	class Node{
+		public:
+			Node* next;
+			int data;
+	};
+	public:
+		int list_length =0;
+		
+		Node* head;
+		Node* curr;
+		Node* temp;
+		//Constructor
+		HeadNode(){
+			this ->head = NULL;
+			this ->curr = NULL;
+			this ->temp = NULL;	
+		}
+		void insert(int data){
+			Node* node = new Node();
+			node->data = data;
+			node->next = NULL;
+			if(head != NULL){
+				curr = head;
+				//Adds nodes to list 
+				while(curr -> next != NULL){
+					curr = curr ->next;
+				}
+				curr-> next = node; 
+				list_length++;
+			}
+			//Adds front node
+			else
+			{
+				head = node;
+				list_length++;
+			}
+		}
+		void remove(int pos,int del){
+			int middle,end;
+		
+			Node* delPtr = NULL;
+			temp = head;
+			curr = head;
+			//Iterates through each node until desired data is reached
+			while(curr != NULL && curr->data != del){
+				temp = curr;
+				curr = curr -> next;
+			}
+			//Deletes first node in list 
+			if(pos == 1){
+				//Initializes the middle and end of list also
+				middle = list_length /2;
+				end = list_length;
+				delPtr = curr;
+				curr = curr -> next;
+				temp -> next = curr; 
+				head= curr;
+				delete delPtr;
+				list_length--;
+				cout<< del<< " was deleted at the Front:"<<endl;
+			}
+			//Deletes node in middle
+			else if(pos == middle){
+				delPtr = curr;
+				curr = curr -> next;
+				temp -> next = curr; 
+				delete delPtr;
+				list_length--;
+				cout<< del<< " was deleted at the middle:"<<endl;	
+			}
+			//Deletes node in end
+			else if(pos == end){
+				delPtr = curr;
+				curr = curr -> next;
+				temp -> next = curr; 
+				delete delPtr;
+				list_length--;
+				cout<< del<< " was deleted at the End:"<<endl;		
+			}
+		}
+		//Tranverses the list and prints it out
+		void print(){
+			for(curr= head; curr !=NULL; curr = curr ->next){
+				cout<< "Element: "<< curr -> data<< ", Length of Linked list: "<<list_length<<endl;
+			}	
+		}
+		void reverse(){
+			temp = head;
+			curr = head;
+			Node* tail; 
+			//Iterates through list until right before reaching null
+			while(curr -> next!= NULL){
+				temp = curr;
+				curr = curr -> next;
+			}
+			//Sets tail to curent pointer
+			tail = curr;
+			cout<<"Element: "<<tail ->data<<", Length of Linked list: "<<list_length<<endl;
+			//Iternates through whole list setting the tail to the temp pointer 
+			// and prints out each element until reaching head node.
+			while(tail !=head){
+				temp = head;
+				curr = head;
+				while(curr -> next!= NULL){
+					temp = curr;
+					curr = curr -> next;
+					if(temp->next== tail){
+						tail =temp;
+						cout<<"Element: "<<tail->data<<", Length of Linked list: "<<list_length<<endl;
+						break;
+					}
+				}
+			}
+		}
+};
 int main(){
-//Creating instance of class	
-	Dog dog;
-//Setting name. type, and age of dog
-	dog.setName("George");
-	dog.setType("Husky");
-	dog.setAge(3);
-
-//Printing out members of class
-	cout<< dog.getName()<<"'s a "<<
-	dog.getType()<<" and is "<<dog.getAge()
-	<<" years old."<<endl;
-	system("PAUSE");
+	cout<<"Ivan Fonseca-Project2"<<endl;
+	cout<<"Applied Programming Languages-Fall 2017"<<endl;
+	cout<<""<<endl;
+	
+	HeadNode headNode;
+	//Adds 10 nodes to linked list
+	for(int i=1;i<=10;i++){
+		headNode.insert(i);
+	}
+	headNode.print();
+	cout<<""<<endl;
+	cout<<"List traversed reversed"<<endl;
+	headNode.reverse();
+	cout<<""<<endl;
+	//Deletes the front, middle, and end Nodes of list
+	for(int i=1;i<=10;i++){
+		headNode.remove(i,i);
+	}
+	headNode.print();
+	cout<<""<<endl;
+	cout<<"List traversed reversed"<<endl;
+	headNode.reverse();
+	return 0;
 }
-
-
--- Project2/readme.md
+++ Project2/readme.md
@@ -1,10 +1,24 @@
-# Project two
+# Ivan Fonseca-Project two
 
-## project2.cpp
-This file contains the main function where the dog class, set, and get
-functions are initiated. At the end the program prints out the 
-dog information.
-## project2.hxx
-This header file contains the dog class itself. Also has the name, type
-and age functions as well.
+## HeadNode Class
+Class contains the Node class that has next and data.
+Also contains the head, curr, and temp pointers that
+are used to interate through the list for the insert,
+remove, print, and reverse functions.
+## Insert function
+This functions inserts elements starting from the beginning
+and links them together.
+## Remove Function
+Checks the postion and data value during iteration. Then
+uses them to delete in the front, middle, and end of the list.
+## print 
+This function traverses the list and prints out each element.
+## Reverse function
+Assigns tail pointer to the end of the list and taverses
+backwards printing each element.
+## Main
+Main function adds 10 elements to list. Also, prints the list 
+traversed and reversed. Then calls the remove function to
+delete the front, middle, and end elements of list. Then prints
+the list traversed and reversed again.
 
